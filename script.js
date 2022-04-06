cba1b2d49c8615c89d91121072b127966b8a1acb
astridsawatzky <astrid.sawatzky@gmx.de>
2022-04-06 21:29:01 +0200

    adjust to the grading specs.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function compute() {
+
     //Get the values and calculate 
     var principal = parseFloat(document.getElementById("principal").value);
     var rate = parseFloat(document.getElementById("rate").value);
@@ -6,16 +7,16 @@ function compute() {
     var interest = principal * years * rate / 100;
     var yearInTheFuture = new Date().getFullYear() + years;
     //Create the Interest text
-    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
         "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
-        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "you will receive an amount of <mark>" + interest + "</mark>,<br/> " +
         "in the year <mark>" + yearInTheFuture + "</mark>";
 
 }
 
-//update ther ate value
+//update the rate value
 function getSliderValue() {
-    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value +"/";
 }
 
 //Check for positive values
