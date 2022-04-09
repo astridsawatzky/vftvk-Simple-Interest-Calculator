e149b812f93624cd5ca127026d3602afc4a04973
astridsawatzky <astrid.sawatzky@gmx.de>
2022-04-09 10:28:59 +0200

    add alert for empty box accept 0.x values

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function calculateInterest(principal, years, rate) {
-    return principal * years * rate / 100;
+
+    return Math.floor(principal * years * rate) / 100;
 }
 module.exports = calculateInterest;
 function compute() {
 
-    //Get the values and calculate 
-    var principal = parseFloat(document.getElementById("principal").value);
-    var rate = parseFloat(document.getElementById("rate").value);
-    var years = parseInt(document.getElementById("years").value);
-    var interest = calculateInterest(principal, years, rate);
-    var yearInTheFuture = new Date().getFullYear() + years;
-    //Create the Interest text
-    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
-        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
-        "you will receive an amount of <mark>" + interest + "</mark>,<br/> " +
-        "in the year <mark>" + yearInTheFuture + "</mark>";
-
+    //Get the values and calculate
+    if(isPrincipalBiggerThanZero()) {
+        var principal = parseFloat(document.getElementById("principal").value);
+        var rate = parseFloat(document.getElementById("rate").value);
+        var years = parseInt(document.getElementById("years").value);
+        var interest = calculateInterest(principal, years, rate);
+        var yearInTheFuture = new Date().getFullYear() + years;
+        //Create the Interest text
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
+            "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+            "you will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+            "in the year <mark>" + yearInTheFuture + "</mark>";
+    } else {
+        alertWrongEntryForPrincipal();
+    }
 }
 
 //update the rate value
@@ -23,13 +27,20 @@ function getSliderValue() {
     document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value +"/";
 }
 
+function isPrincipalBiggerThanZero() {
+    const principal = document.getElementById("principal").value;
+    return parseInt(principal) >= 0;
+}
+
+function alertWrongEntryForPrincipal() {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+}
+
 //Check for positive values
 function validateAmount() {
-    var principal = document.getElementById("principal").value;
-    var biggerThanZero = parseInt(principal) > 0;
-    if (!biggerThanZero) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
+    if (!isPrincipalBiggerThanZero()) {
+        alertWrongEntryForPrincipal();
     }
 
 }
