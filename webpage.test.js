eaf896df1d83392706c3fc31ca09fcbff1da421a
astridsawatzky <astrid.sawatzky@gmx.de>
2022-04-13 06:08:17 +0200

    mark 6 check answer for values

--- webpage.test.js
+++ webpage.test.js
@@ -1,5 +1,5 @@
-
 const puppeteer = require("puppeteer");
+
 describe('marks', () => {
 
     beforeAll(async () => {
@@ -12,14 +12,14 @@ describe('marks', () => {
 
     })
     it('02 Principal input box is present', async () => {
-        var selected = await page.evaluate(() => {
+        const selected = await page.evaluate(() => {
             return document.getElementById("principal");
 
         });
         expect(selected).toBeTruthy();
-       })
+    })
     it('03 Interest slider is present', async () => {
-        var selected = await page.evaluate(() => {
+        const selected = await page.evaluate(() => {
             return document.getElementById("rate");
 
         });
@@ -27,7 +27,7 @@ describe('marks', () => {
     })
 
     it('04 the dropdown box expands to the number of years', async () => {
-        var selected = await page.evaluate(() => {
+        let selected = await page.evaluate(() => {
             return parseInt(document.getElementById("years").value);
 
         });
@@ -42,14 +42,34 @@ describe('marks', () => {
 
     })
     it('05 The compute interest button is present', async () => {
-        var selected = await page.evaluate(() => {
+        const selected = await page.evaluate(() => {
             return document.getElementById("ComputeInterest");
 
         });
         expect(selected).toBeTruthy();
     })
+    it('06 Amount 1000, rate 10, years 10', async () => {
 
-    
+        await page.type('#principal', "1000");
+        await page.evaluate(() => {
+            document.getElementById("rate").value = 10;
+        })
+        await page.select("#years", "10");
+        const beforeText = await page.evaluate(() => {
+            return document.getElementById("result").innerHTML;
+
+        });
+        expect(beforeText).toBe("");
+        await page.click("#ComputeInterest");
+        const afterText = await page.evaluate(() => {
+            return document.getElementById("result").innerHTML;
+
+        });
+        expect(afterText).toBe("If you deposit <mark>1000</mark>,<br>at an interest rate of <mark>10</mark>,<br>you will receive an amount of <mark>1000</mark>,<br> in the year <mark>2032</mark>");
+
+
+    })
+
     it('16 The title of the page is: Web App - Simple Interest Calculator', async () => {
 
         const title = await page.title();
