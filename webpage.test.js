bf29009faf6261a7a5f7db73c1b1bd24b34ee4fa
astridsawatzky <astrid.sawatzky@gmx.de>
2022-04-13 04:48:42 +0200

    tests check for existence of elements for grade 2 3 4 5

--- webpage.test.js
+++ webpage.test.js
@@ -1,10 +1,7 @@
 
 const puppeteer = require("puppeteer");
-describe('Google', () => {
- //     beforeEach (async () => {
- //     browser = await puppeteer.launch({ headless: true });
- //     page = await browser.newPage();
- // })
+describe('marks', () => {
+
     beforeAll(async () => {
         await page.goto('file://C:/github/sick/index.html');
     })
@@ -14,8 +11,20 @@ describe('Google', () => {
         await expect(page).toMatch('Simple Interest Calculator');
 
     })
-    
+    it('02 Principal input box is present', async () => {
+        var selected = await page.evaluate(() => {
+            return document.getElementById("principal");
 
+        });
+        expect(selected).toBeTruthy();
+       })
+    it('03 Interest slider is present', async () => {
+        var selected = await page.evaluate(() => {
+            return document.getElementById("rate");
+
+        });
+        expect(selected).toBeTruthy();
+    })
 
     it('04 the dropdown box expands to the number of years', async () => {
         var selected = await page.evaluate(() => {
@@ -32,7 +41,15 @@ describe('Google', () => {
         expect(selected).toBe(10);
 
     })
+    it('05 The compute interest button is present', async () => {
+        var selected = await page.evaluate(() => {
+            return document.getElementById("ComputeInterest");
 
+        });
+        expect(selected).toBeTruthy();
+    })
+
+    
     it('16 The title of the page is: Web App - Simple Interest Calculator', async () => {
 
         const title = await page.title();
